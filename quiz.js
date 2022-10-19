062a265d166103f9d4b70ee16d97c38219d52ae3
unknown <emina.paljevac@gmail.com>
2022-10-19 21:06:09 +0200

    Shuffle func

--- quiz.js
+++ quiz.js
@@ -1,1 +1,14 @@
+const shuffleQuestions = (questions) => {
+  for (let i = 10000; i > 0; i--) {
+    const j = Math.floor(Math.random() * questions.length);
+    const k = Math.floor(Math.random() * questions.length);
+    const temp = questions[j];
+    questions[j] = questions[k];
+    questions[k] = temp;
+  }
+};
+shuffleQuestions(questions);
 console.log(questions);
+// console.log();
+function createQuestion(question) {}
+createQuestion(question[0]);
